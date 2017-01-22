5f5e8d06da3f4e70f316184cbf70f69f255618f5
Mohamadreza Khostevan <mohamadreza.khostevan@neuland-bfi.de>
2017-01-22 13:16:34 +0100

    Finished preversion for login/registration API

--- bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/authentication/AuthenticationResource.java
+++ bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/authentication/AuthenticationResource.java
@@ -1,24 +1,18 @@
 package org.eclipse.smarthome.io.rest.core.authentication;
 
-import java.security.Key;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.smarthome.core.auth.AuthenticationException;
 import org.eclipse.smarthome.core.auth.UsernamePasswordCredentials;
+import org.eclipse.smarthome.io.rest.JSONResponse;
 import org.eclipse.smarthome.io.rest.SatisfiableRESTResource;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.crypto.MacProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -26,7 +20,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 /**
- * @author Amir Khostevan
+ * @author Mohamadreza Amir Khostevan
  * @author Sven Höper
  */
 @Path(AuthenticationResource.PATH_AUTHENTICATION)
@@ -35,42 +29,59 @@ public class AuthenticationResource implements SatisfiableRESTResource {
 
     /** The URI path to this resource */
     public static final String PATH_AUTHENTICATION = "auth";
+    private String key = "beduinokey";
 
     @POST
     @Path("/register")
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces({ MediaType.APPLICATION_JSON })
     @ApiOperation(value = "Register new account")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Internal Server Error") })
-    public Response register(@FormParam("username") String username, @FormParam("password") String password) {
-        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
-        // UUID uuid = ConfigurationFileHandler.getUUID(credentials);
-        return Response.ok().build();
+            @ApiResponse(code = 400, message = "Required field is missing"),
+            @ApiResponse(code = 409, message = "User already exists") })
+    public Response register(@ApiParam(value = "Username", required = true) @FormParam("username") String username,
+            @ApiParam(value = "Password", required = true) @FormParam("password") String password) {
+        if (!requriedFieldIsMissing(username, password)) {
+            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+            try {
+                // UUID uuid = ConfigurationFileHandler.getUUID(credentials);
+                return Response.ok().build();
+            } catch (AuthenticationException e) {
+                return JSONResponse.createErrorResponse(Status.UNAUTHORIZED, "User already exists");
+            }
+        }
+        return JSONResponse.createErrorResponse(Status.BAD_REQUEST, "Required field is missing.");
     }
 
     @POST
     @Path("/login")
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces({ MediaType.APPLICATION_JSON })
     @ApiOperation(value = "Get JSON Web Token (JWT)")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 403, message = "Invalid Credentials"),
-            @ApiResponse(code = 500, message = "Internal Server Error") })
-    public Response login(@ApiParam(value = "Username", required = true) @QueryParam("username") String username,
-            @ApiParam(value = "Password", required = true) @QueryParam("password") String password) {
-        // TODO Username und Passwort verarbeiten und passende Antwort liefern
-        Key key = MacProvider.generateKey();
+            @ApiResponse(code = 400, message = "Required field is missing"),
+            @ApiResponse(code = 401, message = "Invalid Credentials") })
+    public Response login(@ApiParam(value = "Username", required = true) @FormParam("username") String username,
+            @ApiParam(value = "Password", required = true) @FormParam("password") String password) {
 
-        String compactJws = Jwts.builder().setSubject("Joe").signWith(SignatureAlgorithm.HS512, key).compact();
-        System.out.println(compactJws);
-        Map<String, String> json = new HashMap<String, String>();
-        json.put("key", compactJws);
-        return Response.ok(json).build();
+        if (!requriedFieldIsMissing(username, password)) {
+            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+            // UUID uuid = ConfigurationFileHandler.login(credentials);
+            // if (uuid != null) {
+            // String base64Key = Base64.encodeBase64String(key.getBytes());
+            // JwtBuilder builder = Jwts.builder();
+            // builder.setSubject(uuid.toString());
+            // builder.claim("name", username);
+            // String compactJws = builder.signWith(SignatureAlgorithm.HS512, base64Key).compact();
+            // Map<String, String> json = new HashMap<String, String>();
+            // json.put("key", compactJws);
+            // return Response.ok(json).build();
+            // }
+            return JSONResponse.createErrorResponse(Status.UNAUTHORIZED, "Invalid Credentials");
+        }
+        return JSONResponse.createErrorResponse(Status.BAD_REQUEST, "Required field is missing.");
     }
 
-    private Response authenticationErrorResponse(String message) {
-        return null;
+    private boolean requriedFieldIsMissing(String username, String password) {
+        return username == null || username.isEmpty() || password == null || password.isEmpty();
     }
 
     @Override
